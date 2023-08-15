daf3c27464141486f1626443b35b9b6d65325532
miftakhuljannan <miftakhuljannan04@gmail.com>
2023-08-15 16:02:53 +0700

    new activity

--- app/src/main/java/com/example/tes/MainActivity.java
+++ app/src/main/java/com/example/tes/MainActivity.java
@@ -1,13 +1,5 @@
 package com.example.tes;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -22,34 +14,42 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    FloatingActionButton fab;
-    DrawerLayout drawerLayout;
-    BottomNavigationView bottomNavigationView;
+    private FloatingActionButton fab;
+    private DrawerLayout drawerLayout;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         fab = findViewById(R.id.fab);
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         TextView titleTextView = findViewById(R.id.titleTextView);
         titleTextView.setText("Sekolah Terang Bangsa");
-
 
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
@@ -64,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView.setBackground(null);
         bottomNavigationView.setOnItemSelectedListener(item -> {
-
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
                 replaceFragment(new HomeFragment());
@@ -75,8 +74,6 @@ public class MainActivity extends AppCompatActivity {
             } else if (itemId == R.id.library) {
                 replaceFragment(new LibraryFragment());
             }
-
-
             return true;
         });
 
@@ -86,6 +83,8 @@ public class MainActivity extends AppCompatActivity {
                 showBottomDialog();
             }
         });
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -98,22 +97,65 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.group) {
-            Toast.makeText(this,"membuat group baru",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Create new group", Toast.LENGTH_SHORT).show();
         }
         if (id == R.id.search1) {
-            Toast.makeText(this,"membuat new search",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "New search", Toast.LENGTH_SHORT).show();
         }
         if (id == R.id.settings) {
-            Toast.makeText(this,"membuat settings baru",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Create new settings", Toast.LENGTH_SHORT).show();
         }
         if (id == R.id.profile) {
-            Toast.makeText(this,"membuat profile baru",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Create new profile", Toast.LENGTH_SHORT).show();
         }
         return true;
     }
+    @Override
+        public boolean onNavigationItemSelected (@NonNull MenuItem item){
+        // Handle navigation item selection here
+        Fragment selectedFragment = null;
 
-    //outside
-    private  void replaceFragment(Fragment fragment) {
+        int itemId = item.getItemId(); // Get the ID of the selected menu item
+
+        if (itemId == R.id.nav_home) {
+            selectedFragment = new HomeFragment();
+        } else if (itemId == R.id.nav_kalender) {
+            selectedFragment = new KalenderFragment();
+        } else if (itemId == R.id.nav_share) {
+            selectedFragment = new JadwalPelajaran();
+        } else if (itemId == R.id.nav_about) {
+            selectedFragment = new PembelajaranDigital();
+        } else if (itemId == R.id.nav_Info) {
+            selectedFragment = new Informasi();
+        } else if (itemId == R.id.nav_settings) {
+            selectedFragment = new Settings();
+        } else if (itemId == R.id.nav_logout) {
+            // Implement logout functionality here
+        }
+
+        if (selectedFragment != null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, selectedFragment);
+            transaction.commit();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
@@ -121,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showBottomDialog() {
+        // Dialog implementation here
 
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
