5597d5b261b93f33295d589e8877e403b93a9eae
miftakhuljannan <miftakhuljannan04@gmail.com>
2023-08-23 13:04:57 +0700

    new modif hari ini

--- app/src/main/java/com/example/tes/PembelajaranDigital.java
+++ app/src/main/java/com/example/tes/PembelajaranDigital.java
@@ -1,64 +1,74 @@
 package com.example.tes;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PembelajaranDigital#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PembelajaranDigital extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText nameEditText;
+    private Button addButton;
+    private ListView contactListView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> contacts = new ArrayList<>();
 
     public PembelajaranDigital() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PembelajaranDigital.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PembelajaranDigital newInstance(String param1, String param2) {
-        PembelajaranDigital fragment = new PembelajaranDigital();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static PembelajaranDigital newInstance() {
+        return new PembelajaranDigital();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pembelajaran_digital, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_pembelajaran_digital, container, false);
+
+        nameEditText = rootView.findViewById(R.id.nameEditText);
+        addButton = rootView.findViewById(R.id.addButton);
+        contactListView = rootView.findViewById(R.id.contactListView);
+
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, contacts);
+        contactListView.setAdapter(adapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                if (!name.isEmpty()) {
+                    contacts.add(name);
+                    adapter.notifyDataSetChanged();
+                    nameEditText.setText("");
+                }
+            }
+        });
+
+        contactListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Implement your Edit logic here
+                // For example: start an edit activity/fragment
+            }
+        });
+
+        contactListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                contacts.remove(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+        return rootView;
     }
 }
