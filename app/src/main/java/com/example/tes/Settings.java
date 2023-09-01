7ad3ca41f495f9054c5d37eb9c231b96e0185752
miftakhuljannan <miftakhuljannan04@gmail.com>
2023-09-01 10:39:58 +0700

    button live chat

--- app/src/main/java/com/example/tes/Settings.java
+++ app/src/main/java/com/example/tes/Settings.java
@@ -5,12 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Settings extends Fragment {
+
+    private RecyclerView chatRecyclerView;
+    private EditText messageEditText;
+    private Button liveChatButton;
 
     public Settings() {
         // Required empty public constructor
@@ -25,8 +31,21 @@ public class Settings extends Fragment {
         RadioButton radioButtonOn = rootView.findViewById(R.id.radioButtonOn);
         RadioButton radioButtonOff = rootView.findViewById(R.id.radioButtonOff);
         Button buttonSave = rootView.findViewById(R.id.buttonSave);
+        liveChatButton = rootView.findViewById(R.id.liveChatButton);
+        chatRecyclerView = rootView.findViewById(R.id.chatRecyclerView);
+        messageEditText = rootView.findViewById(R.id.messageEditText);
 
-        // Load notification setting status here and set the appropriate radio button checked
+        // Pengaturan RecyclerView dan Adapter untuk chat
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        chatRecyclerView.setLayoutManager(layoutManager);
+        // Buat adapter ChatAdapter sesuai kebutuhan
+
+        liveChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleChatUI();
+            }
+        });
 
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +62,21 @@ public class Settings extends Fragment {
         });
 
         return rootView;
+    }
+
+    // Metode untuk mengatur visibilitas elemen chat UI
+    private void toggleChatUI() {
+        if (chatRecyclerView.getVisibility() == View.VISIBLE) {
+            chatRecyclerView.setVisibility(View.GONE);
+            messageEditText.setVisibility(View.GONE);
+        } else {
+            chatRecyclerView.setVisibility(View.VISIBLE);
+            messageEditText.setVisibility(View.VISIBLE);
+        }
+    }
+
+    // Metode untuk memulai obrolan
+    public void startChatRoom(View view) {
+        // Implementasi untuk memulai obrolan atau navigasi ke halaman obrolan
     }
 }
