59b8a4529e0037db849f5c646393c6a40bce86e2
miftakhuljannan <miftakhuljannan04@gmail.com>
2023-09-07 15:54:30 +0700

    new input siswa

--- app/src/main/java/com/example/tes/LibraryFragment.java
+++ app/src/main/java/com/example/tes/LibraryFragment.java
@@ -6,10 +6,16 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import java.util.HashMap;
 
 public class LibraryFragment extends Fragment {
@@ -18,6 +24,16 @@ public class LibraryFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+
+    private EditText isinpm;
+    private EditText isinama;
+    private RadioGroup jk;
+    private Spinner kelas;
+    private Spinner agama;
+    private EditText tempatlahir;
+    private EditText tanggallahir;
+    private Button simpan;
+    private TextView hasil;
 
     public LibraryFragment() {
         // Required empty public constructor
@@ -75,6 +91,38 @@ public class LibraryFragment extends Fragment {
                 return true;
             }
             return false;
+        });
+
+        // Inisialisasi elemen-elemen dari layout
+        isinpm = view.findViewById(R.id.isinpm);
+        isinama = view.findViewById(R.id.isinama);
+        jk = view.findViewById(R.id.jk);
+        kelas = view.findViewById(R.id.kelas);
+        agama = view.findViewById(R.id.agama);
+        tempatlahir = view.findViewById(R.id.tempatlahir);
+        tanggallahir = view.findViewById(R.id.tanggallahir);
+        simpan = view.findViewById(R.id.simpan);
+        hasil = view.findViewById(R.id.hasil);
+
+        // Menambahkan event onClickListener ke tombol "SIMPAN"
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mengambil nilai dari elemen-elemen yang diisi oleh pengguna
+                String npm = isinpm.getText().toString();
+                String nama = isinama.getText().toString();
+                String jenisKelamin = ((RadioButton) view.findViewById(jk.getCheckedRadioButtonId())).getText().toString();
+                String kelasSelected = kelas.getSelectedItem().toString();
+                String agamaSelected = agama.getSelectedItem().toString();
+                String tempatLahir = tempatlahir.getText().toString();
+                String tanggalLahir = tanggallahir.getText().toString();
+
+                // Menampilkan hasil input ke dalam TextView "hasil"
+                String hasilInput = "NPM: " + npm + "\nNama: " + nama + "\nJenis Kelamin: " + jenisKelamin
+                        + "\nKelas: " + kelasSelected + "\nAgama: " + agamaSelected + "\nTempat Lahir: " + tempatLahir
+                        + "\nTanggal Lahir: " + tanggalLahir;
+                hasil.setText(hasilInput);
+            }
         });
 
         return view;
